e35288e7d5d229d2e7c986473f99a396c20c2d60
Michael Wood <mwood@data-crush.com>
2024-09-19 19:02:12 -0400

    comparison operators

--- lib/jel.ex
+++ lib/jel.ex
@@ -8,16 +8,28 @@ defmodule Jel do
       {:ok, command} ->
         [{operator, args}] = Map.to_list(command)
 
-        case operator do
-          "+" -> {:ok, Enum.sum(args)}
-          "-" -> {:ok, Enum.reduce(args, fn x, acc -> acc - x end)}
-          "*" -> {:ok, Enum.reduce(args, fn x, acc -> acc * x end)}
-          "/" -> {:ok, Enum.reduce(args, fn x, acc -> div(acc, x) end)}
-          _ -> {:error, "invalid operator"}
-        end
+        Jel.eval(operator, args)
 
-      {:error, error} ->
-        {:error, error}
+      {:error, _} ->
+        {:error, "Invalid JSON"}
+    end
+  end
+
+  def eval(operator, args) do
+    case operator do
+      "+" -> {:ok, Enum.sum(args)}
+      "-" -> {:ok, Enum.reduce(args, fn x, acc -> acc - x end)}
+      "*" -> {:ok, Enum.reduce(args, fn x, acc -> acc * x end)}
+      "/" -> {:ok, Enum.reduce(args, fn x, acc -> div(acc, x) end)}
+      "==" -> {:ok, Enum.at(args, 0) == Enum.at(args, 1)}
+      "!=" -> {:ok, Enum.at(args, 0) != Enum.at(args, 1)}
+      "&&" -> {:ok, Enum.all?(args, fn x -> x end)}
+      "||" -> {:ok, Enum.any?(args, fn x -> x end)}
+      ">" -> {:ok, Enum.at(args, 0) > Enum.at(args, 1)}
+      ">=" -> {:ok, Enum.at(args, 0) >= Enum.at(args, 1)}
+      "<" -> {:ok, Enum.at(args, 0) < Enum.at(args, 1)}
+      "<=" -> {:ok, Enum.at(args, 0) <= Enum.at(args, 1)}
+      _ -> {:error, "invalid operator"}
     end
   end
 end
--- test/jel_test.exs
+++ test/jel_test.exs
@@ -26,8 +26,114 @@ defmodule JelTest do
     assert result == 2
   end
 
+  test "handles `&&` operator false" do
+    {:ok, result} = Jel.eval(~s({"&&": [ true, true, false ] }))
+
+    assert result == false
+  end
+
+  test "handles `&&` operator true" do
+    {:ok, result} = Jel.eval(~s({"&&": [ true, true, true ] }))
+
+    assert result == true
+  end
+
+  test "handles `||` operator false" do
+    {:ok, result} = Jel.eval(~s({"||": [ false, false, false ] }))
+
+    assert result == false
+  end
+
+  test "handles `||` operator true" do
+    {:ok, result} = Jel.eval(~s({"||": [ false, false, true ] }))
+
+    assert result == true
+  end
+
+  test "handles `==` operator true" do
+    {:ok, result} = Jel.eval(~s({"==": [ 2, 2 ] }))
+
+    assert result == true
+  end
+
+  test "handles `==` operator false" do
+    {:ok, result} = Jel.eval(~s({"==": [ 2, 3 ] }))
+
+    assert result == false
+  end
+
+  test "handles `!=` operator true" do
+    {:ok, result} = Jel.eval(~s({ "!=": [ 2, 3 ] }))
+
+    assert result == true
+  end
+
+  test "handles `!=` operator false" do
+    {:ok, result} = Jel.eval(~s({ "!=": [ 2, 2 ] }))
+
+    assert result == false
+  end
+
+  test "handles `>` operator false" do
+    {:ok, result} = Jel.eval(~s({">": [ 2, 3 ] }))
+
+    assert result == false
+  end
+
+  test "handles `>` operator true" do
+    {:ok, result} = Jel.eval(~s({">": [ 3, 2 ] }))
+
+    assert result == true
+  end
+
+  test "handles `>=` operator false" do
+    {:ok, result} = Jel.eval(~s({">=": [ 2, 3 ] }))
+
+    assert result == false
+  end
+
+  test "handles `>=` operator true" do
+    {:ok, result_gt} = Jel.eval(~s({">=": [ 4, 3 ] }))
+    {:ok, result_eq} = Jel.eval(~s({">=": [ 3, 3 ] }))
+
+    assert result_eq == true
+    assert result_gt == true
+  end
+
+  test "handles `<` operator false" do
+    {:ok, result} = Jel.eval(~s({"<": [ 3, 2 ] }))
+
+    assert result == false
+  end
+
+  test "handles `<` operator true" do
+    {:ok, result} = Jel.eval(~s({"<": [ 2, 3 ] }))
+
+    assert result == true
+  end
+
+  test "handles `<=` operator false" do
+    {:ok, result} = Jel.eval(~s({"<=": [ 5, 4 ] }))
+
+    assert result == false
+  end
+
+  test "handles `<=` operator true" do
+    {:ok, result_lt} = Jel.eval(~s({"<=": [ 2, 3 ] }))
+    {:ok, result_eq} = Jel.eval(~s({"<=": [ 3, 3 ] }))
+
+    assert result_eq == true
+    assert result_lt == true
+  end
+
+  test "handles invalid json" do
+    {:error, error} = Jel.eval(~s({"!: [ true ] }))
+
+    assert error == "Invalid JSON"
+  end
+
   test "handles invalid operators" do
-    {:error, result} = Jel.eval(~s({"@@": [ 2, 3, 4] }))
+    {:error, result} = Jel.eval(~s({"@@": [ 2, 3, 4 ] }))
 
     assert result == "invalid operator"
   end
